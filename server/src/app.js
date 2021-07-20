98d32e80425f856970606bf873094a0a5e6f1984
natnael-meresa <natnaelmeresa6@gmail.com>
2021-07-20 13:41:29 +0300

    express app built

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined'))
 app.use(cors())
 
-app.get('/status', (req, res) =>{
-  res.send({
-    message: 'hello man'
-  })
- })
- 
- app.listen(process.env.PORT || 8081)
+app.post('/register', (req,res) => {
+    res.send({
+        message: `Hello ${req.body.email}! welcome`
+    });
+});
+
+
+app.listen(process.env.PORT || 8081, () =>
+  console.log(`Example app listening on port 8081`),
+);
