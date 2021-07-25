9fa72c0eb0c6b7be82e271436c8893a23dd4bec4
natnael-meresa <natnaelmeresa6@gmail.com>
2021-07-25 23:31:17 +0300

    api.js modified to include login and registertion

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,4 +1,7 @@
 const Post = require('../src/models/Article')
+const User = require('../src/models/User')
+const bcrypt = require("bcryptjs")
+const passport = require("passport")
 
 module.exports = class API {
     static async fetchAllArticle(req,res) {
@@ -8,6 +11,58 @@ module.exports = class API {
         } catch (err) {
             res.status(404).json({ message: err.message})
         }
+    }
+
+    static async login(req,res, next) {
+        console.log('hit /login')
+        passport.authenticate("local", function(err, user, info) {
+            console.log( info )
+            if (err) {
+                console.log(err)
+                console.log('hit err 1')
+                return res.status(400).json({ errors:err });
+            }
+
+            if (!user) {
+                return res.status(400).json({errors: "No user found"});
+                console.log('hit err 2')
+            }
+            console.log('hot 2')
+            req.logIn(user, function(err) {
+                console.log('hot')
+                if (err) {
+                    return res.status(400).json({errors: err});
+                    console.log('hit err 3')
+                }
+                console.log('hit success')
+                return res.status(200).json({success: `logged in ${user.id}`});
+
+            });
+        })(req, res, next);
+    }
+
+    static async register(req,res) {
+        User.findOne({ username: req.body.email }, async (err, doc) => {
+            if (err) throw err;
+            if (doc) res.send("User Already Exists");
+            if (!doc) {
+            const hashedPassword = await bcrypt.hash(req.body.password, 10);
+                    // bcrypt.genSalt(10, (err, salt) => {
+                    //     bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    //         if (err) throw err;
+                    //         newUser.password = hash;
+                    //     });
+                    // }); 
+        
+            const newUser = new User({
+                email: req.body.email,
+                username: req.body.username,
+                password: hashedPassword
+            });
+            await newUser.save();
+            res.send("User Created");
+            }
+        });
     }
     static async fetchArticleByID(req,res) {
         res.send("create Article")
