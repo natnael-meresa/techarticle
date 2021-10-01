98e19757b68d3968bcbb05cb7cd0258e4617c0ab
natnael meresa <natnaelmeresa6@gmail.com>
2021-10-01 07:24:46 +0300

    sorry

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -33,7 +33,7 @@ router.get('/me', VerifyToken, function(req, res, next) {
 
 	User.findById(req.userId, {password: 0}, function (err, user) {
 		if (err) return res.status(500).send("There was a problem finding the user.");
-    	if (!user) return res.status(404).send("No user found.");
+    	if (!user) return res.status(404).send("No user found Sorray.");
     
     	res.status(200).send(user);
 	})
