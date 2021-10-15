7ea2ad52a28b9c0133dbf89f4c424e21893d2e58
natnael meresa <natnaelmeresa6@gmail.com>
2021-10-15 15:19:22 +0300

    minor change

--- server/controllers/VerifyToken.js
+++ server/controllers/VerifyToken.js
@@ -2,19 +2,32 @@ const jwt = require('jsonwebtoken')
 const config = require('../config/config')
 
 function verifyToken(req, res, next) {
-	// body...
+	if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        var token =  req.headers.authorization.split(' ')[1];
+        console.log(`token1:- ${token}`)
+    } else if (req.query && req.query.token) {
+        var token = req.query.token;
+        console.log(`token2:- ${token}`)
 
-	var token = req.headers['x-access-token'];
+    }
+	// var token = req.headers['x-access-token'];
 
+	console.log(`token outside:- ${token}`)
 	if(!token)
 		return res.status(403).send({auth: false, message: 'No token provided'});
 
 	jwt.verify(token, config.secret, function(err , decoded){
 		if (err) return res.status(500).send({auth:false, message: 'Failed to authenticate token'});
-	req.userID = decoded.id;
+		console.log(`decoded:- ${decoded.id}`)
+		
+	req.userId = decoded.id;
 
 	next()	
 	})
 }
+
+// function verifyOptional(req, res, next) {
+
+// }
 
 module.exports = verifyToken;
