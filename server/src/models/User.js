04ebadbdccb7df47617b472435110ce29b16aedd
natnael meresa <natnaelmeresa6@gmail.com>
2021-10-28 19:24:13 +0300

    modifiys user model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
 
     // username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
     // email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
-    // bio: String,
+    bio: String,
     // image: String,
     // favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
     // following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
