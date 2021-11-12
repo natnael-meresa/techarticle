15e67892fa63595edc0f3dd3c2e38bdb9c4a97ee
natnael meresa <natnaelmeresa6@gmail.com>
2021-11-12 18:49:07 +0300

    main changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -19,26 +19,7 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style scoped>
 
-.navbar {
-  padding: 30px;
-  background-color: ;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,29 @@
-import { createApp } from 'vue'
+import { createApp, h } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import store from './store'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import axios from 'axios'
 
-createApp(App).use(router).use(router).mount('#app')
+
+
+createApp({
+    created(){
+        const userString = localStorage.getItem('user')
+        if (userString) {
+            const userData = JSON.parse(userString)
+            this.$store.commit('SET_USER_DATA', userData)
+        }
+        axios.interceptors.response.use(
+            response => response,
+            error => {
+                if (error.response.status === 401) {
+                    this.$store.dispatch('logout')
+                }
+                return Promise.reject(error)
+            }
+        )
+    },
+    render: () => h(App),
+  }).use(store).use(router).mount('#app')
