9a2182488989cd8a856fc1fb47da9641fecb4ae8
natnael meresa <natnaelmeresa6@gmail.com>
2021-12-29 20:08:57 +0300

    now db.js

--- server/config/db.js
+++ server/config/db.js
@@ -6,6 +6,7 @@ const connectDB =  async () => {
         const connection = await mongoose.connect(process.env['MONGO_URI'],
             { 
                 useNewUrlParser: true,
+                useCreateIndex: true,
                 useUnifiedTopology: true,
         }); 
 
