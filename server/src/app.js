ae672704bc5463eb435d7a8617d4860732559c55
natnael meresa <natnaelmeresa6@gmail.com>
2021-12-29 12:54:57 +0300

    adds build script to package.json

--- server/src/app.js
+++ server/src/app.js
@@ -52,9 +52,9 @@ app.use("/api/profile", Profile)
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if(process.env.NODE_ENV === 'production' ){
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.use(express.static(path.join(__dirname, '/client/dist')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')))
 }
 
 
